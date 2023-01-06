1bf26511e2b344628990ed26f7c9ddbef5ad7af3
hessam94 <azzurro_94@yahoo.com>
2023-01-06 01:28:57 -0500

    fix process bug

--- src/system.cpp
+++ src/system.cpp
@@ -23,44 +23,20 @@ Processor& System::Cpu() { return cpu_; }
 
 // This is not completely correct, what happens if a process get killed, so we need to create a new processes every time and fill it
 // but for this project i guess it works.
-vector<Process>& System::Processes() {
-    vector<int> Pids = LinuxParser::Pids();
-    set<int> PidsSet(Pids.begin(), Pids.end());
-    for (auto& pr : processes_)
-    {
-        auto it = PidsSet.find(pr.Pid());
-        if (it != PidsSet.end())
-            PidsSet.erase(it);
-    } 
-
-    // rest of the processes, the new ones that was not among them
-    for (auto& pr : PidsSet)
-    {
-        Process p(pr);
-        processes_.push_back(p);
+vector<Process>& System::Processes()
+{
+  vector<int> pids = LinuxParser::Pids();
+  processes_.clear();
+  for (long unsigned int i = 0; i < pids.size(); i++) {
+    if (LinuxParser::Ram(pids[i]) != "0") {
+      Process process(pids[i]);
+      processes_.push_back(process);
     }
+  }
+  std::sort(processes_.begin(), processes_.end());
+  return processes_;
 
-    // this will set the cpuUtil property for all live processes , so there will not be any process in the list
-    // with empty cpuutil_. because we overloaded the operator < on Cpuutil_  
-
-    // the problem is here, having thsi code gives me a runtime error, if I comment this two lines then there is no error
-    // but no meaningful soritng later we have
-    //for (auto& pr: processes_)
-      // pr.CpuUtilization(); 
-
-
-    // simple sort for proceses to have bigger cpuUtil in the beginning 
-     for (int i=0;i< processes_.size();i++)
-        for(int j= i+1;j< processes_.size();j++)
-            if (processes_[i].CpuUtil_ < processes_[j].CpuUtil_)
-                std::swap(processes_[i] , processes_[j]);
-
-     //sort(processes_.begin() , processes_.end());
-     //reverse(processes_.begin() , processes_.end());
-     //sort(processes_.begin() , processes_.end() , std::greater<Process>()) ;
-
-     return processes_; 
-     } 
+}
 
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
